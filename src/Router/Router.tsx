1e722b6dd57e03763865e1b7df807b5122556ef4
Amir <a.presovsky@list.ru>
2024-02-06 21:07:48 +0300

    deleted margin left for namespace

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -3,12 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import { routes } from './Routes'
 
 const Router: FC = () => {
-  const renderRoutes = () => {
-    return routes.map((route, index) => {
-      return <Route key={index} path={route.path} element={route.element} />
-    })
-  }
-
-  return <Routes>{renderRoutes()}</Routes>
+  return (
+    <Routes>
+      {routes.map((route, index) => {
+        return <Route key={index} path={route.path} element={route.element} />
+      })}
+    </Routes>
+  )
 }
 export default Router
--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -1,10 +1,11 @@
 import App from '../Components/Screens/Homepage/App'
 import InfoPage from '../Components/Screens/InfoPage/Info'
 import IdePage from '../Components/Screens/IDEPage/IdePage'
+import { ReactElement } from 'react'
 
 interface IRoute {
   path: string
-  element: JSX.Element
+  element: ReactElement
 }
 
 export const routes: IRoute[] = [
