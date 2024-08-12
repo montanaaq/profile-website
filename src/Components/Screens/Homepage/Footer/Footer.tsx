d43108ef841baf382ada25ac248dad230bd09889
Amir <a.presovsky@list.ru>
2024-08-12 18:33:18 +0300

    2024

--- src/Components/Screens/Homepage/Footer/Footer.tsx
+++ src/Components/Screens/Homepage/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: FC = () => {
   return (
     <footer className="footer">
       <span style={{ opacity: '.6' }}>
-        © 2023&nbsp;
+        © 2024&nbsp;
         <a href="https://montaanaq.netlify.app">Montana</a>
         &nbsp;Powered by&nbsp;
         <a href="https://react.com" target="_blank" rel="noreferrer">
